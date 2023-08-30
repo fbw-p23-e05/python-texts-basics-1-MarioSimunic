21ec789c24332204392796888063e4000a04413e
Mario <mario.simunic@dci-student.org>
2023-08-30 14:16:58 +0200

    Done. Missing Task3

---
+++ Task4.py
@@ -0,0 +1,4 @@
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+if text.find("capital"):
+    print("capital:", text.index("capital"))
